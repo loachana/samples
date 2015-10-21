f00493cc1cea73ea22f0b1ec35d3fbc594984924
lochanaeg <lochanaeg@gmail.com>
2015-10-21 20:00:02 +0530

    update

---
+++ ex2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+    char a;
+    std::cout << "hello world" << std::endl; //use std each time
+    std::cout<<"tell me our name: " << std::cin >> a;
+    std::cout<< a;
+
+    return 0;
+}
